15ff9fc489b4a3dc4674dc00fc8b9c7c0ce27adf
taehopark <qkrxogh7@gmail.com>
2023-07-19 23:23:25 +0900

    알고리즘: 구현문제.자바

--- src/BJ/BJ_10810.java
+++ src/BJ/BJ_10810.java
@@ -1,10 +1,36 @@
 package BJ;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author 박태호
+ * <pre>
+ * 공넣기
+ * </pre>
+ * 
+ * */
 public class BJ_10810 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); int M = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for (int m = 0; m < M; m++) {
+			int i = sc.nextInt();
+			int j = sc.nextInt();
+			int k = sc.nextInt();
+			for(int idx = i; i<=j;i++) {
+				arr[i-1] = k;
+			}
+		}
+		for (int num : arr) {
+			System.out.print(num+" ");
+		}
+		System.out.println();
+//		System.out.println(Arrays.toString(arr));
 	}
 
 }
