8a0ac9bbaf0acccf1fa8df952f16510fcc00b4d6
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-06-18 20:18:46 +0900

    [Gold IV] Title: 타일 채우기, Time: 92 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long[] dp = new long[N + 1];
+        dp[0] = 1;
+        if (N > 1) dp[2] = 3;
+
+        if (N % 2 == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        for (int i = 4; i <= N; i += 2) {
+
+            dp[i] = dp[i - 2] * 3;
+            for (int j = i - 4; j >= 0; j -= 2) {
+                dp[i] += dp[j] * 2;
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
