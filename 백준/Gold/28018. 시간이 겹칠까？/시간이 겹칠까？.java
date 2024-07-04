aa207548c676330c362aa6a8d4c4f1aba5c46ad0
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-07-04 21:17:36 +0900

    [Gold V] Title: 시간이 겹칠까?, Time: 1368 ms, Memory: 68444 KB -BaekjoonHub

---
+++ 백준/Gold/28018. 시간이 겹칠까？/시간이 겹칠까？.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 누적합.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+    
+        int table[] = new int[1000010];
+
+        StringTokenizer st;
+
+        for (int i = 0; i<N;i++){
+            st = new StringTokenizer(bf.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            table[start] ++;
+            table[end+1] --;
+        }
+//        System.out.println(Arrays.toString(table));
+
+        for (int i = 1; i < table.length; i++) {
+            table[i] += table[i-1];
+//        System.out.println(table[i]);
+        }
+//        System.out.println(Arrays.toString(table));
+
+        int Q =  Integer.parseInt(bf.readLine());
+
+//        int[] queryArr = new int[Q];
+
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i<Q;i++){
+            int query = Integer.parseInt(st.nextToken());
+            System.out.println(table[query]);
+            //            queryArr[i] = query;
+        }
+
+
+    }
+}
