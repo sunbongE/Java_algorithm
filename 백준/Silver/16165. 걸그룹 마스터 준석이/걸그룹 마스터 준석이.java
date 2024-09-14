c1e8e7ab6441b293f7e202d2fc2d2f5ab406d7a3
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-09-14 14:24:15 +0900

    [Silver III] Title: 걸그룹 마스터 준석이, Time: 60 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.java
+++ 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.java
@@ -1,49 +1,59 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
-    static int N,M;
+    static int N, M;
+
     public static void main(String[] args) throws IOException {
 
-        HashMap<String, String> info = new HashMap<>(); // k :맴버명, v : 그룹명
+        HashMap<String, String> memberToGroup = new HashMap<>(); // 맴버명 -> 그룹명 저장
+        HashMap<String, List<String>> groupToMembers = new HashMap<>(); // 그룹명 -> 맴버 리스트 저장
         StringBuilder ans = new StringBuilder();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 그룹 수
+        M = Integer.parseInt(st.nextToken()); // 문제 수
 
         for (int n = 0; n < N; n++) {
-            String groupName = br.readLine();
-            int groupCnt = Integer.parseInt(br.readLine());
+            String groupName = br.readLine(); // 그룹명 입력
+            int groupCnt = Integer.parseInt(br.readLine()); // 그룹에 속한 맴버 수 입력
+            List<String> members = new ArrayList<>(); // 그룹의 맴버 리스트
+
             for (int cnt = 0; cnt < groupCnt; cnt++) {
-                String memberName = br.readLine();
-                info.put(memberName, groupName);
+                String memberName = br.readLine(); // 맴버명 입력
+                memberToGroup.put(memberName, groupName); // 맴버명과 그룹명 저장
+                members.add(memberName); // 맴버 리스트에 추가
             }
+
+            // 그룹명으로 맴버 리스트 저장 (사전순 정렬)
+            Collections.sort(members);
+            groupToMembers.put(groupName, members);
         }
 
         for (int m = 0; m < M; m++) {
-            String query = br.readLine();
-            int queryType = Integer.parseInt(br.readLine());
-            if(queryType == 1){ // 그룹명을 찾는다.
-                ans.append(info.get(query)+"\n");
-            }else{ // 그룹에 속한 맴버들을 찾는다.
-                ArrayList<String> nameList = new ArrayList<>();
-                for (Map.Entry<String, String> keyValue : info.entrySet()) {
-                    if(keyValue.getValue().equals(query)){ //
-                        nameList.add(keyValue.getKey()); // 맴버 명을 추출한다.
-                    }
-                }
-                Collections.sort(nameList); // 사전순 정렬.
+            String query = br.readLine(); // 문제 입력
+            int queryType = Integer.parseInt(br.readLine()); // 문제 유형 입력
+
+            if (queryType == 1) { // 맴버명으로 그룹명을 찾는 경우
+                ans.append(memberToGroup.get(query)).append("\n");
+            } else { // 그룹명으로 맴버 리스트를 찾는 경우
+                List<String> nameList = groupToMembers.get(query);
                 for (String s : nameList) {
-                    ans.append(s+"\n");
+                    ans.append(s).append("\n");
                 }
             }
         }
 
-        System.out.println(ans);
+        bw.write(ans.toString());
+        bw.flush(); // 결과 출력
+        bw.close();
+        br.close();
     }
 }
