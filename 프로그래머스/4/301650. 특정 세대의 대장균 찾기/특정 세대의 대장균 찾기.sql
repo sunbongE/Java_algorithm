e544e8509be628e6a6a805214e7b3958b8206fe6
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-09-25 23:01:15 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -0,0 +1,16 @@
+-- 코드를 작성해주세요
+SELECT ID
+FROM ECOLI_DATA
+WHERE PARENT_ID IN 
+    (SELECT ID 
+     FROM ECOLI_DATA
+     WHERE PARENT_ID IN
+        (SELECT ID 
+          FROM ECOLI_DATA
+          WHERE PARENT_ID IS NULL))
+ORDER BY ID;
+
+# SELECT ID 
+#           FROM ECOLI_DATA
+#           WHERE PARENT_ID IS NULL;
+          
