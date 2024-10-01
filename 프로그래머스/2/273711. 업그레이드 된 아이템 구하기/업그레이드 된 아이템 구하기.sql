7ae03bf435af8b0181f71cce72ba35796b061976
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-10-01 15:50:28 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 작성해주세요
+
+SELECT ITEM_ID,	ITEM_NAME,	RARITY
+FROM ITEM_INFO 
+WHERE ITEM_ID IN (
+                    SELECT A.ITEM_ID
+                    FROM ITEM_TREE A, ITEM_INFO B
+                    WHERE A.PARENT_ITEM_ID = B.ITEM_ID AND B.RARITY = 'RARE')
+ORDER BY ITEM_ID DESC 
