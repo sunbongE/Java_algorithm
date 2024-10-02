9a2c7b0cfc2cfa51f7056a91db02b41fcb2c7915
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-10-02 18:35:48 +0900

    [level 2] Title: 전화번호 목록, Time: 301.83 ms, Memory: 98.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,17 @@
+function solution(phone_book) {
+    var answer = true;
+    const map = new Map();
+    for(let i=0;i<phone_book.length;i++){
+        map.set(phone_book[i],0);
+    }
+    
+    for( let tmp of phone_book){
+        for(let i =1 ; i<tmp.length;i++){
+            if(map.has(tmp.substr(0,i))){
+                return false;
+            }
+        }
+    }
+    
+    return answer;
+}
