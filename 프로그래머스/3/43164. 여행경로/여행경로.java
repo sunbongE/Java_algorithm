738bf9e983ffc24fa75887ee4a857726af38ea30
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-10-15 00:47:58 +0900

    [level 3] Title: 여행경로, Time: 0.90 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Solution {
+    // 출발지와 도착지의 우선순위를 관리하는 HashMap
+    HashMap<String, PriorityQueue<Ticket>> info = new HashMap<>();
+    ArrayList<String> answer;
+    final String START = "ICN"; // 시작점
+    
+    public String[] solution(String[][] tickets) {
+        answer = new ArrayList<>();
+        
+        // 정보를 순회하면서 출발지와 도착지 정보를 입력한다.
+        for (String[] ticket : tickets) {
+            String from = ticket[0];  // 출발지
+            String to = ticket[1];    // 도착지
+            
+            // 출발지가 이미 존재하면 해당 출발지의 우선순위 큐에 추가
+            if (info.containsKey(from)) {
+                info.get(from).offer(new Ticket(to, false));
+            } else {
+                // 출발지가 없다면 새로운 우선순위 큐를 만들어서 추가
+                PriorityQueue<Ticket> pq = new PriorityQueue<>();
+                pq.offer(new Ticket(to, false));
+                info.put(from, pq);
+            }
+        }
+        
+        // DFS 방식으로 경로 탐색
+        dfs(START);
+        
+        // ArrayList를 배열로 변환하여 반환
+        return answer.toArray(new String[0]);
+    }
+    
+    // DFS 방식의 경로 탐색
+    private void dfs(String cur) {
+        // 현재 위치에서 가능한 모든 경로를 순차적으로 탐색 (사전순으로 자동 정렬됨)
+        while (info.containsKey(cur) && !info.get(cur).isEmpty()) {
+            Ticket nextTicket = info.get(cur).poll();
+            dfs(nextTicket.to);  // 재귀적으로 다음 경로 탐색
+        }
+        // 경로를 역순으로 쌓아가야 하므로 마지막에 추가
+        answer.add(0, cur);
+    }
+
+    // 티켓 정보를 담은 클래스
+    public class Ticket implements Comparable<Ticket> {
+        String to;       // 도착지
+        boolean isUsed;  // 사용 여부 (DFS에서 경로 추적용)
+
+        public Ticket(String to, boolean isUsed) {
+            this.to = to;
+            this.isUsed = isUsed;
+        }
+
+        // 도착지를 사전순으로 정렬하기 위한 compareTo 메서드
+        @Override
+        public int compareTo(Ticket o) {
+            return this.to.compareTo(o.to);
+        }
+    }
+}
