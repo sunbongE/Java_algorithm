bb2f00552054a97f2866dc371e2411906cee7863
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-11-09 22:35:45 +0900

    [Gold I] Title: K번째 수, Time: 112 ms, Memory: 12904 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+
+        int l = 1;
+        int r = k;
+        int result = 0;
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            long count = 0;
+
+            for(int i = 1; i <= n; i++) {
+                count += Math.min(mid / i, n);
+            }
+//            System.out.println("========="+l+", "+r+"=========");
+//            System.out.println("mid : "+mid);
+//            System.out.println("count : "+count);
+            if(count < k) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+                result = mid;
+            }
+        }
+         System.out.println(result);
+    }
+}
+
