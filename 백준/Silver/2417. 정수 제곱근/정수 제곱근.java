3eea8ed9ae27ffcc200ff07db3fbbd77dbcf904c
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-11-16 22:40:51 +0900

    [Silver IV] Title: 정수 제곱근, Time: 64 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
@@ -1,37 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
- 
- 
+
 public class Main {
-	
-	public static long N;
-	public static long answer = 0;
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	N = Long.parseLong(st.nextToken());
-    	
-    	binarySearch();
-    	System.out.println(answer);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long a = Long.parseLong(br.readLine());
+
+        long s=0;
+        long e = a;
+//        System.out.println((long)Math.sqrt(Math.pow(2,63)));
+        while(s<=e){
+            long q = (s+e)/2;
+
+            if(Math.pow(q,2) < a){
+                s = q+1;
+            }else{
+                e = q-1;
+            }
+        }
+
+        System.out.println(s);
+
     }
-    
-    public static void binarySearch() {
-    	long start = 0;
-    	long end = N;
-    	
-    	while(start <= end) {
-    		long middle = ( start + end ) / 2;
-    		    		
-    		if( Math.pow(middle, 2) >= N ) {
-    			answer = middle;
-    			end = middle - 1;
-    		} else  {
-    			start = middle + 1;
-    		}
-    	}
-    }
- 
 }
