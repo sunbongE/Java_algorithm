96caf3232ab818014ba456d6d7408597364ff9fd
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-12-02 18:24:49 +0900

    [Silver I] Title: 극장 좌석, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.java
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.java
@@ -3,4 +3,37 @@ import java.util.*;
 
 public class Main {
 
-    static int dp[] = new int [41];		public static void main(String[] args) throws Exception{		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		int T = Integer.parseInt(br.readLine());		dp[0] = 1;		dp[1] = 1; 		dp[2] = 2;		for(int i = 3; i<=T; i++){			dp[i] = dp[i-1]+dp[i-2];		}		int n = Integer.parseInt(br.readLine());		int ans = 1;		int start =0; 		int stop =0;		for(int i=0; i<n; i++){			stop = Integer.parseInt(br.readLine());			ans *= dp[stop-start-1];			start = stop;		}		ans *= dp[T-stop];		System.out.println(ans);	} }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int ans=1;
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[41];
+
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
+        dp[4]=5;
+
+        for (int i = 5; i <= n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        int[] arr = new int[m];
+        
+        for (int i = 0; i < m; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int pre=0;
+        for(Integer vip: arr){
+            ans *= dp[vip-1-pre];
+            pre = vip;
+        }
+        ans *= dp[n-pre];
+        System.out.println(ans);
+
+    }
+}
