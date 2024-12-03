f761e83dae6d27b6a3fdc573423233e175039ef0
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-12-03 17:02:54 +0900

    [Silver I] Title: 계단, Time: 212 ms, Memory: 27292 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/21600. 계단/계단.java
+++ 백준/Silver/21600. 계단/계단.java
@@ -13,10 +13,17 @@ public class Main {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
-        int l=1; int maxL = 0;
+        int l=0; int maxL = 0;
         for (int i = 0; i < n; i++) {
-            if(l > A[i]) l = A[i];
-            else l++;
+            // 히스토 그램이 더 크면 계단 높이 올리기.
+            if(l+1 <= A[i]){
+                l++;
+            }else{
+                l = A[i];
+            }
+            // 작으면 계단 높이를 현재 히스토 그램 높이에 맞추기.
+
+
             maxL = Math.max(l,maxL);
 
         }
