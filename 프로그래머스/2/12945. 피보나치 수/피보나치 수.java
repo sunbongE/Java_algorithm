642bd8c722c7feced18b848cedb232c139f84e46
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2024-12-23 19:20:19 +0900

    [level 2] Title: 피보나치 수, Time: 2.60 ms, Memory: 90.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    final int MOD = 1234567;
+    public int solution(int n) {
+        int answer = 0;
+        int[] fibo = new int[n+1];
+        fibo[0] =0;
+        fibo[1]=1;
+        for(int i=2; i<=n;i++){
+            fibo[i] = (fibo[i-2]+fibo[i-1])%MOD;
+        }
+        return fibo[n];
+    }
+}
