d454b03de560fb47c299cd02b46062f3f03c2240
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2025-01-03 16:33:49 +0900

    [Silver II] Title: 상자넣기, Time: 80 ms, Memory: 11904 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -1,33 +1,40 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
+/**
+ * dp
+ * 1. arr[i]보다 이전 값중에 arr[i]보다 작은 수
+ * 2. 1에서 사용한 인덱스 기준으로 dp[i]가 최대인 값에 +1 한 값
+ * 3. dp[i]의 최대값을 ans에 갱신.
+ *
+ * **/
 public class Main {
     public static void main(String[] args) throws IOException {
+        int ans =1 ;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 배열 길이 입력
-        int[] arr = new int[n]; // 배열 초기화
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        int[] dp = new int[n];
 
-        int[] dp = new int[n]; // DP 배열
-        Arrays.fill(dp, 1); // 모든 값을 1로 초기화
-        int ans = 1; // 최소 길이는 항상 1 이상
+        Arrays.fill(dp,1);
 
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i] && dp[i] < dp[j] + 1) { // 증가하는 부분수열 조건 확인
-                    dp[i] = dp[j] + 1; // dp 갱신
+            int cur = arr[i];
+            for (int j = i-1; j >= 0; j--) {
+                if(arr[j] < cur && dp[i] < dp[j]+1){
+                    dp[i] = dp[j]+1;
+                    ans = Math.max(ans, dp[i]);
                 }
             }
-            ans = Math.max(ans, dp[i]); // 최댓값 갱신
         }
-
-        System.out.println(ans); // 최장 길이 출력
+//        System.out.println(Arrays.toString(dp));
+        System.out.println(ans );
     }
 }
