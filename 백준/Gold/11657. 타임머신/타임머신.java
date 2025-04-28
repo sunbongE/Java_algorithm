0eafbeb4b9f5e7bade64aa01538acb9d7c55126e
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2025-04-28 16:26:44 +0900

    [Gold IV] Title: 타임머신, Time: 224 ms, Memory: 17292 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.*;
+/**
+ *  N개의 도시가 있다. 그리고 한 도시에서 출발하여 다른 도시에 도착하는 버스가 M개 있다.
+ *  각 버스는 A, B, C로 나타낼 수 있는데,
+ *  A는 시작도시, B는 도착도시, C는 버스를 타고 이동하는데 걸리는 시간이다.
+ *  시간 C가 양수가 아닌 경우가 있다. C = 0인 경우는 순간 이동을 하는 경우, C < 0인 경우는 타임머신으로 시간을 되돌아가는 경우이다.
+ *
+ * 1번 도시에서 출발해서 나머지 도시로 가는 가장 빠른 시간을 구하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 도시의 개수 N (1 ≤ N ≤ 500), 버스 노선의 개수 M (1 ≤ M ≤ 6,000)이 주어진다.
+ * 둘째 줄부터 M개의 줄에는 버스 노선의 정보 A, B, C (1 ≤ A, B ≤ N, -10,000 ≤ C ≤ 10,000)가 주어진다.
+ *
+ * 출력
+ * 만약 1번 도시에서 출발해 어떤 도시로 가는 과정에서 시간을 무한히 오래 전으로 되돌릴 수 있다면 첫째 줄에 -1을 출력한다.
+ * 그렇지 않다면 N-1개 줄에 걸쳐 각 줄에 1번 도시에서 출발해 2번 도시, 3번 도시, ..., N번 도시로 가는 가장 빠른 시간을 순서대로 출력한다.
+ * 만약 해당 도시로 가는 경로가 없다면 대신 -1을 출력한다.
+ *
+ * [벨만포드] O(500*6000 -> 3*10^6) 1초 이내 가능.
+ *  음의 사이클이 존재하면 -1
+ *  아니면 출발점 1에서 나머지로 가는 최소값을 모두 출력.
+ *
+ * */
+public class Main {
+    static StringBuilder ans;
+    static int n,m,INF=Integer.MAX_VALUE;
+    static long[] time;
+    static List<Bus> busInfo;
+    public static void main(String[] args) throws IOException {
+  //       BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ans = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        time=new long[n+1];
+        busInfo = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            busInfo.add(new Bus(a,b,c));
+        }
+
+        bellmanFord(1);
+        System.out.println(ans);
+
+
+
+
+    }
+
+    private static void bellmanFord(int start) {
+        Arrays.fill(time,INF);
+        time[start] = 0;// 시작 값 설정.
+
+        boolean isImpossible=false;
+
+        for (int i = 0; i < n; i++) {
+            for(Bus cur : busInfo){
+                int from = cur.from;
+                int to = cur.to;
+                int t = cur.time;
+                if(time[from] == INF) continue; // 못가는 곳은 무시
+                long nextTime = time[from] + (long) t;
+                if(nextTime < time[to]){
+                    time[to] = nextTime;
+                    if(i == n-1) isImpossible=true;
+                }
+            }
+        }
+
+        if(isImpossible){
+            ans.append(-1);
+        }else{
+            for (int i = 2; i < time.length; i++) {
+                if(time[i]==INF) ans.append(-1).append("\n");
+                else ans.append(time[i]).append("\n");
+            }
+        }
+    }
+
+    static class Bus{
+        int from,to,time;
+        public Bus(int from, int to, int time){
+            this.from = from;
+            this.to=to;
+            this.time = time;
+        }
+    }
+}
