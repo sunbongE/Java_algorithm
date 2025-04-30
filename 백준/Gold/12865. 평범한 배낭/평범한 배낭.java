ff611aa71ba5fb842abd50705f43b83592059c33
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2025-04-30 14:42:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 51400 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,39 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
+ 
 public class Main {
+    static int n,k,w,v;
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      //  BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        int[] W = new int[n];
-        int[] V = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(bf.readLine());
-            W[i] = Integer.parseInt(st.nextToken());
-            V[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int[][] dp = new int[n + 1][k + 1];
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        int[] W = new int[n+1], V = new int[n+1];
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= k; j++) {
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+            W[i] = w;
+            V[i] = v;
+        }
 
-                if(j>= W[i-1]){
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-W[i-1]] + V[i-1]);
+        int[][] dp = new int[n+1][k+1];
+
+        for (int i = 1; i <= n; i++) {
+            int curW = W[i];
+            int curV = V[i];
+            for (int j = 1; j <= k; j++) {
+                if(j >= curW){
+                    dp[i][j] = Math.max(dp[i-1][j-curW]+curV, dp[i-1][j]);
                 }else{
                     dp[i][j] = dp[i-1][j];
                 }
-
-
             }
-
+//            System.out.println(Arrays.toString(dp[i]));
         }
         System.out.println(dp[n][k]);
 
