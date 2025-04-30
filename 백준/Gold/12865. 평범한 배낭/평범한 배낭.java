6c8b50b262aba113e007db67db14c916c08b95c5
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2025-04-30 14:56:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 116 ms, Memory: 13044 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,11 +1,15 @@
 import java.io.*;
 import java.util.*;
- 
+/**
+ * 1. 조합을 구함
+ * 2. 선택된 위치를 기준으로 거리가 최대인 값을 찾는다.
+ *
+ * */
 public class Main {
     static int n,k,w,v;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      //  BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  //       BufferedReader br = new BufferedReader(new FileReader("test.txt"));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
@@ -19,22 +23,37 @@ public class Main {
             W[i] = w;
             V[i] = v;
         }
+//
 
-        int[][] dp = new int[n+1][k+1];
+        int[] dp = new int[k+1];
 
         for (int i = 1; i <= n; i++) {
             int curW = W[i];
             int curV = V[i];
-            for (int j = 1; j <= k; j++) {
+            for (int j = k; j >0 ; j--) {
                 if(j >= curW){
-                    dp[i][j] = Math.max(dp[i-1][j-curW]+curV, dp[i-1][j]);
-                }else{
-                    dp[i][j] = dp[i-1][j];
+                    dp[j] = Math.max(dp[j-curW]+curV, dp[j]);
                 }
             }
-//            System.out.println(Arrays.toString(dp[i]));
-        }
-        System.out.println(dp[n][k]);
+//            System.out.println(Arrays.toString(dp));
 
+        }
+        System.out.println(dp[k]);
+
+//        int[][] dp = new int[n+1][k+1];
+//
+//        for (int i = 1; i <= n; i++) {
+//            int curW = W[i];
+//            int curV = V[i];
+//            for (int j = 1; j <= k; j++) {
+//                if(j >= curW){
+//                    dp[i][j] = Math.max(dp[i-1][j-curW]+curV, dp[i-1][j]);
+//                }else{
+//                    dp[i][j] = dp[i-1][j];
+//                }
+//            }
+//            System.out.println(Arrays.toString(dp[i]));
+//        }
+//        System.out.println(dp[n][k]);
     }
 }
