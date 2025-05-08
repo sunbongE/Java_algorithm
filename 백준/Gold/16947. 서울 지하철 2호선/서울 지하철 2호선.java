ffb426b58c325ce87ca0458282ce100c829a4571
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2025-05-08 15:07:19 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 136 ms, Memory: 15228 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 /**
- * 문제 주석추가
+ * 문제
  * 2호선
  *
  * 1. 사이클이 발생하는 부분이 순환선인데 이걸 찾는다.
@@ -11,7 +11,7 @@ import java.util.*;
  * */
 public class Main {
 
-    static int n ;
+    static int n ,vv[];
     static ArrayList<Integer>[] graph;  // 무향그래프
     static LinkedList<Integer> cycleTmp; // 사이클 발생하는 과정을 기록
     static HashSet<Integer> cycleSet ; // 사이클에 포함된거만 저장
@@ -19,14 +19,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- //        BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+    //     BufferedReader br = new BufferedReader(new FileReader("test.txt"));
 
         n = Integer.parseInt(br.readLine());
         graph = new ArrayList[n+1];
         for (int i = 1; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
-        cycleTmp = new LinkedList<>(); 
+        cycleTmp = new LinkedList<>();
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -40,16 +40,9 @@ public class Main {
         boolean[] v = new boolean[n+1];
         dfs(0,1,v); // 사이클 찾아서 set에 저장
 
-        int[] vv = new int[n+1]; // 방문체크겸 거리체크함.
+        vv = new int[n+1]; // 방문체크겸 거리체크함.
         Arrays.fill(vv,-1);
-        for (int i = 1; i <= n; i++) {
-
-            if(cycleSet.contains(i)){ // 순환선이면,
-                vv[i]=0;
-            }else{// 순환선이 아닌경우 순환선까지의 거리 찾기
-                vv[i] = bfs(i);// 최단경로로 찾아야함.
-            }
-        }
+        bfs();// 최단경로로 찾아야함.
 
         // 답 갱신.
         StringBuilder ans = new StringBuilder();
@@ -60,30 +53,27 @@ public class Main {
         System.out.println(ans);
     }
 
-    private static int bfs(int start) {
+    private static void bfs() {
         Queue<Integer> q = new ArrayDeque<>(); // LinkedList보다 ArrayDeque 성능이 더 좋다고함..메모리도 절약한다고 함..
-        int[] v = new int[n+1];
-        Arrays.fill(v,-1); // 거리체크할거니까 초기화함.
-        v[0] = 0; v[start]=0;
-
-        q.offer(start); // 출발
+        for(Integer c : cycleSet){
+            vv[c] = 0;
+            q.offer(c);
+        }
 
         while(!q.isEmpty()){
             int cur = q.poll();
             if(cycleSet.contains(cur)){ // 순환선에 도착한거임.
-                return v[cur];
+                vv[cur] = 0; // 거리 저장.
             }
             // 인접한 곳으로 이동
             for(Integer next : graph[cur]){
-                if(v[next] == -1){ // 미방문인경우 이동.
+                if(vv[next] == -1){ // 미방문인경우 이동.
                     q.offer(next);
-                    v[next] = v[cur]+1; // 이전 위치 +1 해서 이동 거리를 기록.
+                    vv[next] = vv[cur]+1; // 이전 위치 +1 해서 이동 거리를 기록.
                 }
             }
         }
 
-        // 여기로 올 수 없음.
-        return -1;
     }
 
     private static void dfs(int pre, int cur, boolean[] v) {
