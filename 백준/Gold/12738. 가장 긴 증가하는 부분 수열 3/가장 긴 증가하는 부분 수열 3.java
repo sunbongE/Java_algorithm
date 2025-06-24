55c674065edebc6cfcb00351d7e38995b28b8f3f
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2025-06-24 15:25:13 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 580 ms, Memory: 181196 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -9,7 +9,7 @@ public class Main {
     static int n,m,arr[],ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- //       BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+  //      BufferedReader br = new BufferedReader(new FileReader("test.txt"));
         n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         arr = new int[n+1];
@@ -19,19 +19,33 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        li.add(arr[0]);
-        for (int i = 1; i < n; i++) {
-            int cur = arr[i];
-            int idx = Collections.binarySearch(li,cur);
-            if(idx < 0 ) {
-                idx = -idx - 1;
-            }
 
-            if(li.size() == idx) li.add(cur);
+        for (int i = 0; i < n; i++) {
+            int cur= arr[i];
+            int idx = customBS(li,cur);
+            if(idx == li.size()) li.add(cur);
             else li.set(idx,cur);
-//            System.out.println(idx);
 
         }
+//        System.out.println(li);
+
         System.out.println(li.size());
         }
+
+    private static int customBS(ArrayList<Integer> li, int target) {
+
+        int s = 0, e =li.size()-1;
+        while(s<=e){
+            int mIdx = (s+e)/2;
+
+            if(li.get(mIdx) == target){
+                return mIdx;
+            }else if(li.get(mIdx) > target){
+                e = mIdx-1;
+            }else{
+                s = mIdx+1;
+            }
+        }
+        return s;
+    }
 }
