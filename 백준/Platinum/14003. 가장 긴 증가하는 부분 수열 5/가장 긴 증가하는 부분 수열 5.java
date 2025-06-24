fe7a11daf8eb065b86cfe1390f9fce3487c5f26e
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2025-06-24 17:40:22 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 908 ms, Memory: 319228 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -2,52 +2,76 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    //    BufferedReader br = new BufferedReader(new FileReader("test.txt"));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+    static int n, arr[];
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+        n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] lisVal = new int[n]; // LIS의 끝값 저장
-        int[] pos = new int[n];    // 각 길이별 마지막 인덱스 저장
-        int[] pre = new int[n];    // 경로 추적용
-
-        int len = 0;
+        // lis 값과 인덱스, dp 및 trace 초기화
+        ArrayList<Integer> lis = new ArrayList<>();
+        ArrayList<Integer> lisIdx = new ArrayList<>();
+        int[] dp = new int[n];       // arr[i]가 LIS 몇 번째 위치에 들어갔는지
+        int[] trace = new int[n];    // arr[i] 앞 원소 인덱스 저장
 
         for (int i = 0; i < n; i++) {
             int cur = arr[i];
-            int idx = Arrays.binarySearch(lisVal, 0, len, cur);
-            if (idx < 0) idx = -idx - 1;
+            int idx = customBS(lis, cur); // 들어갈 위치
 
-            lisVal[idx] = cur;
-            pos[idx] = i; // 이 길이의 마지막 원소는 i번째
-            pre[i] = (idx > 0) ? pos[idx - 1] : -1;
+            if (idx == lis.size()) {
+                lis.add(cur);
+                lisIdx.add(i); // 이 길이의 마지막 원소는 i
+            } else {
+                lis.set(idx, cur);
+                lisIdx.set(idx, i);  // 덮어쓰기
+            }
 
-            if (idx == len) len++;
+            dp[i] = idx;
+            trace[i] = (idx > 0) ? lisIdx.get(idx - 1) : -1;
         }
-//        System.out.println("lisVal : "+Arrays.toString(lisVal));
-//        System.out.println("pos    : "+Arrays.toString(pos));
-//        System.out.println("pre    : "+Arrays.toString(pre));
 
-        // 역추적
-        int traceIdx = pos[len - 1];
+        // 역추적 시작: dp[i] == lis.size() - 1 인 마지막 인덱스 찾기
+        int cur = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (dp[i] == lis.size() - 1) {
+                cur = i;
+                break;
+            }
+        }
+
         Stack<Integer> stack = new Stack<>();
-        while (traceIdx != -1) {
-            stack.push(arr[traceIdx]);
-            traceIdx = pre[traceIdx];
+        while (cur != -1) {
+            stack.push(arr[cur]);
+            cur = trace[cur];
         }
 
         // 출력
         StringBuilder sb = new StringBuilder();
-        sb.append(len+"\n");
+        sb.append(lis.size()).append("\n");
         while (!stack.isEmpty()) {
-            sb.append(stack.pop() + " ");
+            sb.append(stack.pop()).append(" ");
         }
         System.out.println(sb);
+    }
+
+    // lower_bound (>= target) 위치 찾기
+    private static int customBS(ArrayList<Integer> li, int target) {
+        int s = 0, e = li.size() - 1;
+        while (s <= e) {
+            int m = (s + e) / 2;
+            if (li.get(m) >= target) {
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+        return s;
     }
 }
