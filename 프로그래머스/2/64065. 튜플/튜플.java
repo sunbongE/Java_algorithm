6ba1186cded6be07df126231f3dc6502d8703de3
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2025-07-18 11:59:34 +0900

    [level 2] Title: 튜플, Time: 56.70 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,64 +1,19 @@
-/*
-s를 일단 배열 형태로 변경
-1. } 기준으로 나눔
-2. 나눈걸 문자열 배열에 기록
-3. 들어간 배열 순회하며, 중괄호와 쉼표 삭제.
-4. 
-*/
+ 
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
-        boolean isOpen      =false;
+        Set<String> set = new HashSet<>();
         
-        ArrayList<ArrayList<Integer>> li = new ArrayList<>();
-        for(int i=0; i<1000000;i++){
-            li.add(new ArrayList<>());
-        }
-
-        int idx=0;
-        String snum = "";
+        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
         
-        for(int i=0; i<s.length();i++){
-            char cur = s.charAt(i);
-            
-            if(cur == '{'  ) { 
-                isOpen = true;
-            } else if(cur == '}' ) { // 닫는데 숫자가 있으면 
-                if(!snum.equals("")) {
-                    li.get(idx).add(Integer.parseInt(snum));
-                    snum="";
-                }
-                isOpen = false;
-                idx++;
-            } else if( cur == ',') { 
-                if(!snum.equals("")){
-                    li.get(idx).add(Integer.parseInt(snum)); // 이전까지 기록된 숫자 넣기
-                    snum = ""; // 초기화
-                }
-            } else {
-                if(isOpen) snum+=cur;
+        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        for(String s1 : arr) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
             }
         }
-        
-        Collections.sort(li, Comparator.comparingInt(List::size));
-        
-        ArrayList<Integer> result = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
-        for(int i =0 ; i< li.size(); i++) {
-            if(li.get(i).isEmpty()) continue;
-            for(Integer num : li.get(i)){
-                if(set.add(num)){
-                    result.add(num);
-                }
-            }
-        }
-        // System.out.println(result);
-        answer = new int[result.size()];
-        for(int i=0; i<result.size();i++){
-            answer[i] = result.get(i);
-        }
-        
         return answer;
     }
 }
