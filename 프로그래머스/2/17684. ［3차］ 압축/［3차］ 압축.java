4ae49feb69fed52b5f3dbf313154da490ae8b557
Taeho Park <108655043+sunbongE@users.noreply.github.com>
2025-07-31 11:18:05 +0900

    [level 2] Title: [3차] 압축, Time: 21.07 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,49 +1,35 @@
 import java.util.*;
 class Solution {
-//    public static void main(String[] args) {
-//        Solution s = new Solution();
-//        int[] answer = s.solution("ABABABABABABABAB");
-////        int[] answer = s.solution("KAKAO");
-//
-//            System.out.println(Arrays.toString(answer) );
-//
-//
-//        System.out.println(map);
-//    }
-    static HashMap<String,Integer> map;
     public int[] solution(String msg) {
-        int[] answer = {};
-        ArrayList<Integer> li = new ArrayList<>();
-        map = new HashMap<>();
+        // LZW 압축 결과를 저장할 리스트
+        List<Integer> result = new ArrayList<>();
 
-        for(int i=1; i<27;i++){
-            char c = (char)('A'+i-1);
-            map.put(""+c,i);
+        // A~Z 사전을 초기화
+        Map<String, Integer> dictionary = new HashMap<>();
+        for (int i = 1; i <= 26; i++) {
+            dictionary.put(String.valueOf((char)('A' + i - 1)), i);
         }
 
-        int idx = 27;
-        int s=0,e=1;
+        int dictIndex = 27; // 새로운 사전 인덱스
+        String word = ""; // 현재 탐색 중인 문자열
 
-        String tmp = ""+msg.charAt(s);
-        for(int i=s+1; i < msg.length();i++){
-            if(map.containsKey(tmp + msg.charAt(i))){
-                tmp += msg.charAt(i);    // 있으면, 다음 긴 문자열 찾음.
-            }else{
-                //없으면,
-                li.add(map.get(tmp));
-                map.put((tmp+msg.charAt(i)),idx++);
-                s = i;
-                tmp = ""+msg.charAt(s);
+        for (int i = 0; i < msg.length(); i++) {
+            word += msg.charAt(i); // 다음 글자 추가
+
+            // 사전에 없다면, 이전 문자열까지만 출력하고 새로운 문자열 등록
+            if (!dictionary.containsKey(word)) {
+                dictionary.put(word, dictIndex++); // 새로운 문자열 사전에 추가
+                result.add(dictionary.get(word.substring(0, word.length() - 1))); // 이전 문자열 출력
+                word = String.valueOf(msg.charAt(i)); // 현재 문자부터 다시 시작
             }
         }
-        li.add(map.get(tmp));
 
-
-
-        answer = new int[li.size()];
-        for(int i=0;i<li.size();i++){
-            answer[i] = li.get(i);
+        // 마지막 단어도 결과에 추가
+        if (!word.isEmpty()) {
+            result.add(dictionary.get(word));
         }
-        return answer;
+
+        // List<Integer> → int[] 변환
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
